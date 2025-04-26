49e9fac4a5204a37374996b05e76b69bec108b44
lstevensN <lstevens@student.neumont.edu>
2025-04-25 19:26:26 -0600

    Added "origin" values

--- src/hitbox.lua
+++ src/hitbox.lua
@@ -1,12 +1,12 @@
 Hitbox = {}
 Hitbox.__index = Hitbox
 
-local function CreateHitboxMesh(w, h)
+local function CreateHitboxMesh(ox, oy)
     local vertices = {
-        {  w/2, h/2, 0,0, 1,1,1,1 },
-        {  w/2,-h/2, 0,0, 1,1,1,1 },
-        { -w/2,-h/2, 0,0, 1,1,1,1 },
-        { -w/2, h/2, 0,0, 1,1,1,1 }
+        {  ox, oy, 0,0, 1,1,1,1 },
+        {  ox,-oy, 0,0, 1,1,1,1 },
+        { -ox,-oy, 0,0, 1,1,1,1 },
+        { -ox, oy, 0,0, 1,1,1,1 }
     }
 
     return love.graphics.newMesh( vertices, "fan", "static" )
@@ -19,7 +19,7 @@ end
 function Hitbox:draw()
     love.graphics.setColor( 20/255, 200/255, 30/255, 0.4 )  -- Set color to Green
 
-    love.graphics.draw( self.mesh, self.x, self.y, self.rotation, self.sx, self.sy )
+    love.graphics.draw( self.mesh, self.x, self.y, self.rotation, self.scaleX, self.scaleY )
 
     love.graphics.setColor( 0, 0, 0, 1 )  -- Set color to White
 end
@@ -32,15 +32,18 @@ function Hitbox:new(x, y, w, h, sx, sy)
         width = w or 1,
         height = h or 1,
 
+        originX = w / 2,
+        originY = h / 2,
+
         rotation = 0,
 
-        sx = sx or 1,
-        sy = sy or 1,
+        scaleX = sx or 1,
+        scaleY = sy or 1,
 
         mesh = {}
     }
 
-    hitbox.mesh = CreateHitboxMesh( hitbox.width, hitbox.height )
+    hitbox.mesh = CreateHitboxMesh( hitbox.originX, hitbox.originY )
 
     setmetatable(hitbox, self)
 
